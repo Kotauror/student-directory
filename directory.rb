da3b8a5b78a9896654561cfad92d906cf6d94b74
kotpsot <zygmunt.justyna@gmail.com>
2018-01-10 17:59:52 +0000

    add gets.chomp, pass result of one method to another method

--- directory.rb
+++ directory.rb
@@ -1,16 +1,33 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+# students = [
+#   {name: "Dr. Hannibal Lecter", cohort: :november},
+#   {name: "Darth Vader", cohort: :november},
+#   {name: "Nurse Ratched", cohort: :november},
+#   {name: "Michael Corleone", cohort: :november},
+#   {name: "Alex DeLarge", cohort: :november},
+#   {name: "The Wicked Witch of the West", cohort: :november},
+#   {name: "Terminator", cohort: :november},
+#   {name: "Freddy Krueger", cohort: :november},
+#   {name: "The Joker", cohort: :november},
+#   {name: "Joffrey Baratheon", cohort: :november},
+#   {name: "Norman Bates", cohort: :november}
+# ]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  # get the first name from the user
+  name = gets.chomp
+  # while the name is not empty (someone input sth), repeat this code
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  students
+end
+
+
 
 def print_header
   puts "The students of Villains Academy"
@@ -27,6 +44,9 @@ def print_footer(names)
   print "Overall, we have #{names.count} great students"
 end
 
+# the output of input_students method (it returns students) is assigned to a
+# new variable that we will use in the functions below 
+people = input_students()
 print_header
-print_students(students)
-print_footer(students)
+print_students(people)
+print_footer(people)
