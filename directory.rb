0a7d2abadeca883f8ab8f94870577be9fbc38888
kotpsot <zygmunt.justyna@gmail.com>
2018-01-15 14:29:10 +0100

    add student directory refactored

--- directory.rb
+++ directory.rb
@@ -102,29 +102,29 @@ people.each { |hash|
 # The input will be given to you as a string? How will you convert it to a symbol?
 # What if the user makes a typo?
 
-def input_students_two_values
-  students = []
-  puts "To finish hit return two times"
-  puts "put the name of the student"
-  name = gets.chomp.to_s
-  puts "put the cohort of the student"
-  cohort = gets.chomp.to_s
-  while !name.empty? || !cohort.empty? do
-    if name.empty? then
-      name = "Name not given".to_sym
-    end
-    if cohort.empty? then
-      cohort = "Cohort not given".to_sym
-    end
-    students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} students"
-    puts "put thename of the student"
-    name = gets.chomp.to_sym
-    puts "put the cohort of the student"
-    cohort = gets.chomp.to_sym
-  end
-  students
-end
+# def input_students_two_values
+#   students = []
+#   puts "To finish hit return two times"
+#   puts "put the name of the student"
+#   name = gets.chomp.to_s
+#   puts "put the cohort of the student"
+#   cohort = gets.chomp.to_s
+#   while !name.empty? || !cohort.empty? do
+#     if name.empty? then
+#       name = "Name not given".to_sym
+#     end
+#     if cohort.empty? then
+#       cohort = "Cohort not given".to_sym
+#     end
+#     students << {name: name, cohort: cohort}
+#     puts "Now we have #{students.count} students"
+#     puts "put thename of the student"
+#     name = gets.chomp.to_sym
+#     puts "put the cohort of the student"
+#     cohort = gets.chomp.to_sym
+#   end
+#   students
+# end
 
 # ----------------------- Exercise 8 -----------------------
 # Once you complete the previous exercise, change the way the users are displayed:
@@ -152,70 +152,160 @@ people.each { |hash|
 # How can you fix it so that it used singular form when appropriate and plural
 # form otherwise?
 
-def input_students_two_values
-  students = []
-  puts "To finish hit return two times"
-  puts "put the name of the student"
-  name = gets.chomp.to_s
-  puts "put the cohort of the student"
-  cohort = gets.chomp.to_s
-  while !name.empty? || !cohort.empty? do
-    if name.empty? then
-      name = "Name not given".to_sym
-    end
-    if cohort.empty? then
-      cohort = "Cohort not given".to_sym
-    end
-    students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} student#{students.count > 1 ? "s" : ""}"
-    puts "put thename of the student"
-    name = gets.chomp.to_sym
-    puts "put the cohort of the student"
-    cohort = gets.chomp.to_sym
-  end
-  students
-end
+# def input_students_two_values
+#   students = []
+#   puts "To finish hit return two times"
+#   puts "put the name of the student"
+#   name = gets.chomp.to_s
+#   puts "put the cohort of the student"
+#   cohort = gets.chomp.to_s
+#   while !name.empty? || !cohort.empty? do
+#     if name.empty? then
+#       name = "Name not given".to_sym
+#     end
+#     if cohort.empty? then
+#       cohort = "Cohort not given".to_sym
+#     end
+#     students << {name: name, cohort: cohort}
+#     puts "Now we have #{students.count} student#{students.count > 1 ? "s" : ""}"
+#     puts "put thename of the student"
+#     name = gets.chomp.to_sym
+#     puts "put the cohort of the student"
+#     cohort = gets.chomp.to_sym
+#   end
+#   students
+# end
 
 # ----------------------- Exercise 10 -----------------------
 # We've been using the chomp() method to get rid of the last return character.
 # RETURN - NOWA LINIA - also a character
 # Find another method among those provided by the String class that could be used
 # for the same purpose (although it will require passing some arguments).
-def input_students_two_values
-  students = []
-  puts "To finish hit return two times"
-  puts "put the name of the student"
-  name = gets.chomp.to_sym #chomp - removes new lines (without chomp the input will never be empty and we will never end the form)
-  puts "put the cohort of the student"
-  cohort = gets.rstrip.to_sym # rstrip - removes whitespaces and empty lines
-  while !name.empty? || !cohort.empty? do
-    if name.empty? then
-      name = "Name not given".to_sym
-    end
-    if cohort.empty? then
-      cohort = "Cohort not given".to_sym
-    end
-    students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} student#{students.count > 1 ? "s" : ""}"
-    puts "put thename of the student"
-    name = gets.chomp.to_sym
-    puts "put the cohort of the student"
-    cohort = gets.chomp.to_sym
-  end
-  students
-end
+
+# def input_students_two_values
+#   students = []
+#   puts "To finish hit return two times"
+#   puts "put the name of the student"
+#   name = gets.chomp.to_sym #chomp - removes new lines (without chomp the input will never be empty and we will never end the form)
+#   puts "put the cohort of the student"
+#   cohort = gets.rstrip.to_sym # rstrip - removes whitespaces and empty lines
+#   while !name.empty? || !cohort.empty? do
+#     if name.empty? then
+#       name = "Name not given".to_sym
+#     end
+#     if cohort.empty? then
+#       cohort = "Cohort not given".to_sym
+#     end
+#     students << {name: name, cohort: cohort}
+#     puts "Now we have #{students.count} student#{students.count > 1 ? "s" : ""}"
+#     puts "put thename of the student"
+#     name = gets.chomp.to_sym
+#     puts "put the cohort of the student"
+#     cohort = gets.chomp.to_sym
+#   end
+#   students
+# end
+
 # ----------------------- Exercise 12 -----------------------
 # What happens if the user doesn't enter any students?
 # It will try to print an empty list. How can you use an if statement
 # (Control Flow) to only print the list if there is at least one student in there?
 
+# def input_students_two_values
+#   students = []
+#   puts "To finish hit return two times"
+#   puts "put the name of the student"
+#   name = gets.chomp.to_sym #chomp - removes new lines (without chomp the input will never be empty and we will never end the form)
+#   puts "put the cohort of the student"
+#   cohort = gets.rstrip.to_sym # rstrip - removes whitespaces and empty lines
+#   while !name.empty? || !cohort.empty? do
+#     if name.empty? then
+#       name = "Name not given".to_sym
+#     end
+#     if cohort.empty? then
+#       cohort = "Cohort not given".to_sym
+#     end
+#     students << {name: name, cohort: cohort}
+#     puts "Now we have #{students.count} student#{students.count > 1 ? "s" : ""}"
+#     puts "put thename of the student"
+#     name = gets.chomp.to_sym
+#     puts "put the cohort of the student"
+#     cohort = gets.chomp.to_sym
+#   end
+#   if students.count > 0
+#     return @students
+#   end
+# end
+
+# ----------------------- STEP 9 - interactive menu -----------------------
+# What happens if the user doesn't enter any students?
+# It will try to print an empty list. How can you use an if statement
+# (Control Flow) to only print the list if there is at least one student in there?
+
+
+# def interactive_menu
+#   students = []
+#   loop do
+#     puts "1. Input the students"
+#     puts "2. Show the students"
+#     puts "9. Exit"
+#     # 2. read the input and save it into a variable
+#     selection = gets.chomp
+#     # 3. do what the user has asked
+#     case selection
+#       when "1"
+#         students = input_students_two_values
+#       when "2"
+#         print_header
+#         print_students(students)
+#         print_footer(students)
+#       when "9"
+#         exit # this will cause the program to terminate
+#       else
+#         puts "I don't know what you meant, try again"
+#     end
+#   end
+# end
+
+# ----------------------- Refactoring of interactive menu -----------------------
+
+
+@students = []
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+
+def print_menu
+puts
+puts "1. Input the students"
+puts "2. Show the students"
+puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students_two_values
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
 def input_students_two_values
-  students = []
-  puts "To finish hit return two times"
-  puts "put the name of the student"
-  name = gets.chomp.to_sym #chomp - removes new lines (without chomp the input will never be empty and we will never end the form)
-  puts "put the cohort of the student"
-  cohort = gets.rstrip.to_sym # rstrip - removes whitespaces and empty lines
+puts "To finish hit return two times"
+puts "put the name of the student"
+name = gets.chomp.to_sym #chomp - removes new lines (without chomp the input will never be empty and we will never end the form)
+puts "put the cohort of the student"
+cohort = gets.rstrip.to_sym # rstrip - removes whitespaces and empty lines
   while !name.empty? || !cohort.empty? do
     if name.empty? then
       name = "Name not given".to_sym
@@ -223,14 +313,39 @@ def input_students_two_values
     if cohort.empty? then
       cohort = "Cohort not given".to_sym
     end
-    students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} student#{students.count > 1 ? "s" : ""}"
+    @students << {name: name, cohort: cohort}
+    puts "Now we have #{@students.count} student#{@students.count > 1 ? "s" : ""}"
     puts "put thename of the student"
     name = gets.chomp.to_sym
     puts "put the cohort of the student"
     cohort = gets.chomp.to_sym
   end
-  if students.count > 0
-    puts students
+  if @students.count > 0
+    return @students
   end
 end
+
+def show_students
+print_header
+print_students
+print_footer
+end
+
+def print_header
+puts "The students of Villains Academy"
+puts "-------------"
+end
+
+def print_students # with while loop
+student_analyzed = 0
+while student_analyzed < @students.length do
+  puts "#{@students[student_analyzed][:name]} #{@students[student_analyzed][:cohort]} cohort"
+  student_analyzed += 1
+end
+end
+
+def print_footer
+print "Overall, we have #{@students.length} great students"
+end
+
+interactive_menu()
